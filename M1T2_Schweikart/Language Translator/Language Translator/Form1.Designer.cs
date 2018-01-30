4aa5382213904ddc66203f4541a3158c69175f2a
BSchweikart <31232164+BSchweikart@users.noreply.github.com>
2018-01-29 22:40:49 -0500

    Add files via upload

---
+++ M1T2_Schweikart/Language Translator/Language Translator/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace Language_Translator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.instructionLabel = new System.Windows.Forms.Label();
+            this.translationLabel = new System.Windows.Forms.Label();
+            this.italianbutton = new System.Windows.Forms.Button();
+            this.spanishButton = new System.Windows.Forms.Button();
+            this.germanButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // instructionLabel
+            // 
+            this.instructionLabel.AutoSize = true;
+            this.instructionLabel.Location = new System.Drawing.Point(39, 21);
+            this.instructionLabel.Name = "instructionLabel";
+            this.instructionLabel.Size = new System.Drawing.Size(344, 20);
+            this.instructionLabel.TabIndex = 0;
+            this.instructionLabel.Text = "Select a Language and I will say Good Morning.";
+            // 
+            // translationLabel
+            // 
+            this.translationLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.translationLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.translationLabel.Location = new System.Drawing.Point(43, 69);
+            this.translationLabel.Name = "translationLabel";
+            this.translationLabel.Size = new System.Drawing.Size(340, 36);
+            this.translationLabel.TabIndex = 1;
+            this.translationLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // italianbutton
+            // 
+            this.italianbutton.Location = new System.Drawing.Point(43, 143);
+            this.italianbutton.Name = "italianbutton";
+            this.italianbutton.Size = new System.Drawing.Size(95, 30);
+            this.italianbutton.TabIndex = 2;
+            this.italianbutton.Text = "Italian";
+            this.italianbutton.UseVisualStyleBackColor = true;
+            this.italianbutton.Click += new System.EventHandler(this.italianbutton_Click);
+            // 
+            // spanishButton
+            // 
+            this.spanishButton.Location = new System.Drawing.Point(164, 143);
+            this.spanishButton.Name = "spanishButton";
+            this.spanishButton.Size = new System.Drawing.Size(95, 30);
+            this.spanishButton.TabIndex = 3;
+            this.spanishButton.Text = "Spanish";
+            this.spanishButton.UseVisualStyleBackColor = true;
+            this.spanishButton.Click += new System.EventHandler(this.spanishButton_Click);
+            // 
+            // germanButton
+            // 
+            this.germanButton.Location = new System.Drawing.Point(288, 143);
+            this.germanButton.Name = "germanButton";
+            this.germanButton.Size = new System.Drawing.Size(95, 30);
+            this.germanButton.TabIndex = 4;
+            this.germanButton.Text = "German";
+            this.germanButton.UseVisualStyleBackColor = true;
+            this.germanButton.Click += new System.EventHandler(this.germanButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(436, 212);
+            this.Controls.Add(this.germanButton);
+            this.Controls.Add(this.spanishButton);
+            this.Controls.Add(this.italianbutton);
+            this.Controls.Add(this.translationLabel);
+            this.Controls.Add(this.instructionLabel);
+            this.Name = "Form1";
+            this.Text = "Language Translator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label instructionLabel;
+        private System.Windows.Forms.Label translationLabel;
+        private System.Windows.Forms.Button italianbutton;
+        private System.Windows.Forms.Button spanishButton;
+        private System.Windows.Forms.Button germanButton;
+    }
+}
+
---
+++ M1T2_Schweikart/Language Translator/Language Translator/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/**
+* 01/29/2018
+* CSC 153
+* Brian Schweikart
+* Language Translator
+*/
+
+namespace Language_Translator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void italianbutton_Click(object sender, EventArgs e)
+        {
+            // translation into Italian upon button press.
+            translationLabel.Text = "Buongiorno";
+        }
+
+        private void spanishButton_Click(object sender, EventArgs e)
+        {
+            // translation into Spanish upon button press.
+            translationLabel.Text = "Buenos Dias";
+
+        }
+
+        private void germanButton_Click(object sender, EventArgs e)
+        {
+            // translation into German upon button press.
+            translationLabel.Text = "Guten Morgen";
+        }
+    }
+}
