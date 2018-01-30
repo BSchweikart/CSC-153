61958ddec2105a9dfe2803a47df556c5f7d46099
BSchweikart <31232164+BSchweikart@users.noreply.github.com>
2018-01-29 22:43:03 -0500

    Add files via upload

---
+++ MT1_Schweikart/Hello World/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+/** 
+ * 01/18/2018
+ * CSC 153
+ * Brian Schweikart
+ * Hello World
+ */
+
+namespace Hello_World
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
